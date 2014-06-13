07c6371284fbb49d55490a9173f8b71675c92c93
szy <532164656@qq.com>
2014-06-13 15:57:32 +0800

    音乐处理cpp文件

---
+++ MusicAnalysis.cpp
@@ -0,0 +1,21 @@
+#include "MusicAnalysis.h"
+
+MusicAnalysis::MusicAnalysis(){
+
+}
+
+MusicAnalysis::~MusicAnalysis(){
+
+}
+
+void MusicAnalysis::pcmAnalysis(){
+
+}
+
+void MusicAnalysis::onsetsAnalysis(){
+
+}
+
+void MusicAnalysis::setPcmData(){
+
+}
