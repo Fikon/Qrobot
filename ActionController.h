edfc3e5b20ebf9b307062af668efc47a79063e6c
fikon <kiky.jiang@gmail.com>
2014-06-24 16:05:12 +0800

    remove some file useless

---
+++ ActionController.h
@@ -0,0 +1,21 @@
+/*
+ *\file ActionController.h
+ *
+ *\brief Qrobot高级动作控制类
+ *
+ *在基本动作的基础上，定义了一系列的高级动作，当上层模块传入
+ *节奏等信息的时候，可以根据所传入的信息表现出相应的动作
+ *
+ *\author 蒋慧强
+ *
+ */
+
+#ifndef ACTION_C0NTROLLER_H
+#define ACTION_CONTROLLER_H
+
+class ActionController
+{
+ 
+};
+
+#endif
